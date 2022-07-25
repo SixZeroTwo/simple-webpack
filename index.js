e7586cd7920030a92b3204334423e56f0b065eaa
SixZeroTwo <454690182@qq.com>
2022-07-25 15:24:38 +0800

    实现根据入口文件创建图

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import fs from 'fs'
 import parser from '@babel/parser'
 import traverse from '@babel/traverse'
-function createAsset() {
+import path from 'path'
+function createAsset(filePath) {
   //读取文件内容，解码成字符串
-  const source = fs.readFileSync('./examples/main.js', {
+  const source = fs.readFileSync(filePath, {
     encoding: 'utf-8'
   })
   //将文件内容转换成抽象语法树
@@ -15,10 +16,25 @@ function createAsset() {
     }
   })
   return {
+    filePath,
     source,
     deps
   }
 }
 
-const asset = createAsset()
-console.log(asset);
+//创建图
+function createGraph() {
+  const mainAsset = createAsset('./examples/main.js')
+  const queue = [mainAsset]
+  for (let asset of queue) {
+    const relativePaths = asset.deps
+    for (let relativePath of relativePaths) {
+      const child = createAsset(path.resolve('./examples/', relativePath))
+      queue.push(child)
+    }
+  }
+
+  return queue
+}
+
+const graph = createGraph()
